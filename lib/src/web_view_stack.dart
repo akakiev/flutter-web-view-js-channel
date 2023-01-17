612e239fba5a1dabde3f4cdf1df50c70f9098eef
LUKASIEWICZ\serhii.baraban <serhii.baraban@pit.lukasiewicz.gov.pl>
2023-01-17 11:19:44 +0100

    registered the NavigationDelegate callback to block navigation to YouTube.com

--- lib/src/web_view_stack.dart
+++ lib/src/web_view_stack.dart
@@ -32,6 +32,20 @@ class _WebViewStackState extends State<WebViewStack> {
             loadingPercentage = 100;
           });
         },
+        onNavigationRequest: (navigation) {
+          final host = Uri.parse(navigation.url).host;
+          if (host.contains('youtube.com')) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  'Blocking navigation to $host',
+                ),
+              ),
+            );
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
       ),
     );
   }
